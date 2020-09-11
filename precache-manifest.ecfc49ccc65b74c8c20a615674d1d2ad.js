8171d986136dede69eac43c45ad7fb6e30fdf6e9
Emmanuel Elikwu <elikwuemmanuel@gmail.com>
2020-09-11 18:24:51 +0100

    Updates

---
+++ precache-manifest.ecfc49ccc65b74c8c20a615674d1d2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ebd6606aff1b3ca83ab31d4cccc9fe",
+    "url": "/react-github-users/index.html"
+  },
+  {
+    "revision": "7bb6ac5208ad8b6c1583",
+    "url": "/react-github-users/static/css/main.535ce99f.chunk.css"
+  },
+  {
+    "revision": "cbd533ed89660e9e622e",
+    "url": "/react-github-users/static/js/2.8fb8c09e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-users/static/js/2.8fb8c09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb6ac5208ad8b6c1583",
+    "url": "/react-github-users/static/js/main.c526dfe3.chunk.js"
+  },
+  {
+    "revision": "b39e77899cdaff5bea52",
+    "url": "/react-github-users/static/js/runtime-main.cc994b05.js"
+  }
+]);
